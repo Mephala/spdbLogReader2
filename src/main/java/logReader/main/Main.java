302dae715a95c4860af88cc2ab21d16996d70762
Mephala <gokhan.ozgozen@gmail.com>
2015-12-24 10:01:25 +0200

    Initial commit

---
+++ src/main/java/logReader/main/Main.java
@@ -0,0 +1,12 @@
+package logReader.main;
+
+/**
+ * Created by GokhanOzgozen on 12/24/2015.
+ */
+public class Main {
+
+
+    public static void main(String[] args){
+        System.out.println("Cebik");
+    }
+}
